9ae5aa45f9b8b5ad6cf59201d09560355abb5e15
Kevin Gomes <kevinmg50@gmail.com>
2025-05-20 20:16:45 +0000

    Add BotConfig and LavalinkNodeConfig interfaces for application configuration

---
+++ src/types/Config.ts
@@ -0,0 +1,30 @@
+export interface LavalinkNodeConfig {
+  id: string;
+  host: string;
+  port: number;
+  password: string;
+  secure: boolean;
+}
+
+export interface BotConfig {
+  bot: {
+    token: string;
+    prefix: string;
+    useSharding: boolean;
+    shards: number;
+    ownerIds: string[];
+  };
+  lavalink: {
+    nodes: LavalinkNodeConfig[];
+    defaultSearchEngine: string;
+    autoPlay: boolean;
+  };
+  cloudflare: {
+    zoneId: string;
+    apiToken: string;
+  };
+  pterodactyl: {
+    url: string;
+    apiKey: string;
+  };
+}
