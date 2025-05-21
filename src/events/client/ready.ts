e1b42491f4b6ad63527512c6f5a3dbe694d78d9f
Kevin Gomes <kevinmg50@gmail.com>
2025-05-21 23:46:38 +0000

    Enhance 'ready' event handler to log server count and set bot presence

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -1,12 +1,24 @@
+import { Client } from 'discord.js';
 import { Event } from '../../types/Event';
 import { logger } from '../../utils/logger';
 
 const event: Event<'ready'> = {
   name: 'ready',
   once: true,
-  execute(client) {
-    logger.success(`Bot online como ${client.user?.tag}`);
-  },
+  execute(client: Client) {
+    logger.success('Bot conectado e pronto como ' + client.user?.tag);
+    logger.info('Servindo para ' + client.guilds.cache.size + ' servidores');
+    
+    client.user?.setPresence({
+      status: 'online',
+      activities: [
+        {
+          name: 'mi!help | Servindo música para ' + client.guilds.cache.size + ' servidores',
+          type: 3 // 3 = Watching
+        }
+      ]
+    });
+  }
 };
 
 export default event;
